be48234aef8c69d5d1c9a5546f79d174dd70b264
onchari <onchariwycliffe@gmail.com>
2018-05-11 16:56:09 +0300

    First

---
+++ OneTouchElectronix/Models/MainCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OneTouchElectronix.Models
+{
+    public class MainCategory
+    {
+        public int MainCategoryId { get; set; }
+
+        [Display(Name ="Main Category Name")]
+        public string MainCategoryName { get; set; }
+    }
+}
