b100c5fb96d1bf080c353837090545153bd473b6
onchari <onchariwycliffe@gmail.com>
2018-05-11 15:48:05 +0300

    Add project files.

---
+++ OneTouchElectronix/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OneTouchElectronix.Startup))]
+namespace OneTouchElectronix
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
