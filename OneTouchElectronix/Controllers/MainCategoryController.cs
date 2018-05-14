2bee9d8bff61f604618956559382490b15b7fb40
onchari <onchariwycliffe@gmail.com>
2018-05-14 12:23:37 -0700

    Monday night

--- OneTouchElectronix/Controllers/MainCategoryController.cs
+++ OneTouchElectronix/Controllers/MainCategoryController.cs
@@ -2,18 +2,98 @@
 using OneTouchElectronix.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
 namespace OneTouchElectronix.Controllers
 {
-    public class MainCategoryController : Controller, IControllers
+    public class MainCategoryController : Controller, IController
     {
 
 
         ApplicationDbContext db = new ApplicationDbContext();
-       
+
+        //POST : MAinCategory/Delete/
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            MainCategory mainCategory = db.MainCategories.Find(id);
+            db.MainCategories.Remove(mainCategory);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        //GET : MainCategory/Delete /1
+        public ActionResult Delete(int ? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MainCategory mainCategory = db.MainCategories.Find(id);
+            if(mainCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mainCategory);
+        }
+
+
+        //POST : MainCategory/Edit/
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit([Bind(Include ="MainCategoryId, MainCategoryName")] MainCategory mainCategoryEntity)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(mainCategoryEntity).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(mainCategoryEntity);
+        }
+
+        //GET : MainCategory/ Edit/
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MainCategory main = db.MainCategories.Find(id);
+            if (main == null)
+            {
+                return HttpNotFound();
+            }
+            return View(main);
+        }
+
+        //GET : Students/Details/5
+        public ActionResult Details(int? id)
+        {
+            
+                       
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MainCategory mainCategory = db.MainCategories.Find(id);
+
+            if (mainCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mainCategory);
+        }
+
+        
+
 
         // GET: MainCategory
         public ActionResult Index()
@@ -43,20 +123,9 @@ namespace OneTouchElectronix.Controllers
             return View(mainCategoryentity);
         }
 
-        public ActionResult Delete()
-        {
-            throw new NotImplementedException();
-        }
-
-        public ActionResult Details()
-        {
-            throw new NotImplementedException();
-        }
-
-        public ActionResult Edit()
-        {
-            throw new NotImplementedException();
-        }
+       
+        
+       
 
         public ActionResult DeleteConfirmed()
         {
